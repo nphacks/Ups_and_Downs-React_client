7090cf8ed53a79a93cdd8c12eb9e03029ca84b81
nphacks <164674563+nphacks@users.noreply.github.com>
2025-01-12 14:31:46 -0500

    Deployed Modus server

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@
 // src/config/config.ts
 
 export const config = {
-    MODUS_API_URL: 'http://localhost:8686/graphql',
+    MODUS_API_URL: 'https://ups-and-downs-modus-app-ups-and-downs.hypermode.app/graphql',
     // Add other configuration variables
   };
   
